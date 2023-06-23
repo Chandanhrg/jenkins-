cbfdbccfc895b948e4ce34638d25f3dc668d5e8c
Chandanhrg <135774141+Chandanhrg@users.noreply.github.com>
2023-06-23 08:10:43 +0530

    Update big2.c

--- big2.c
+++ big2.c
@@ -7,13 +7,13 @@ biggest2()
     printf("Enter two integers :");
     scanf("%d%d", &num1, &num2);
 
-    if (num1 < num2)
+    if (num2 < num1)
     {
-        printf("Largest number is %d.", num2);
+        printf("Largest number is %d.", num1);
     }
     else
     {
-        printf("Largest number is %d.", num1);
+        printf("Largest number is %d.", num2);
     }
 
     //return 0;
